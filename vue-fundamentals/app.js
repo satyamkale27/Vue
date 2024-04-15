08b6c573b4382843655e018a28f27bdb7d4c6569
satyam kale <satyamkale271@gmail.com>
2024-04-15 19:58:52 +0530

    comments

--- vue-fundamentals/app.js
+++ vue-fundamentals/app.js
@@ -6,11 +6,11 @@ Vue.createApp({
     };
   },
 }).mount("#app");
-Vue.createApp({
-  data() {
-    return {
-      firstName: "jane",
-      lastName: "doe",
-    };
-  },
-}).mount("#app2");
+// Vue.createApp({
+//   data() {
+//     return {
+//       firstName: "jane",
+//       lastName: "doe",
+//     };
+//   },
+// }).mount("#app2");
