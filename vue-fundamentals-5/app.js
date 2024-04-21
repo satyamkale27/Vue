b1162925995442eb331686e84292a64e4c8dca66
satyam kale <satyamkale271@gmail.com>
2024-04-21 16:43:27 +0530

    added comment as information to vue lifecycle

--- vue-fundamentals-5/app.js
+++ vue-fundamentals-5/app.js
@@ -9,12 +9,14 @@ let vm = Vue.createApp({
   },
   created() {
     console.log("Created() function called", this.message);
+    // if data is created then only created() function will be called no matter it is assigned or not to component //
   },
   beforeMount() {
     console.log("BeforeMount() function called", this.$el);
   },
   mounted() {
     console.log("mounted() function called", this.$el);
+    // if the data is assigned to component then only mounted() function will be called
   },
 });
 vm.mount("#app");
