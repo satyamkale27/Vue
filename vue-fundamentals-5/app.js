2032ac62d017884d052bb26401ca78d33632e5fc
satyam kale <satyamkale271@gmail.com>
2024-04-21 16:38:44 +0530

    vue lifecycle

--- vue-fundamentals-5/app.js
+++ vue-fundamentals-5/app.js
@@ -4,6 +4,18 @@ let vm = Vue.createApp({
       message: "Hello world!",
     };
   },
+  beforeCreate() {
+    console.log("BeforeCreate() function clalled", this.message);
+  },
+  created() {
+    console.log("Created() function called", this.message);
+  },
+  beforeMount() {
+    console.log("BeforeMount() function called", this.$el);
+  },
+  mounted() {
+    console.log("mounted() function called", this.$el);
+  },
 });
 vm.mount("#app");
 // mounting seprately to look clean and easy to read //
