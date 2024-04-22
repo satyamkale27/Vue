63c4cd2d2066cc2f0ee0b45ce8ed755254a02412
satyam kale <satyamkale271@gmail.com>
2024-04-22 23:03:04 +0530

    vue without compiler

--- vue-fundamentals-5/app.js
+++ vue-fundamentals-5/app.js
@@ -4,20 +4,18 @@ let vm = Vue.createApp({
       message: "Hello world!",
     };
   },
-  beforeCreate() {
-    console.log("BeforeCreate() function clalled", this.message);
-  },
-  created() {
-    console.log("Created() function called", this.message);
-    // if data is created then only created() function will be called no matter it is assigned or not to component //
-  },
-  beforeMount() {
-    console.log("BeforeMount() function called", this.$el);
-  },
-  mounted() {
-    console.log("mounted() function called", this.$el);
-    // if the data is assigned to component then only mounted() function will be called
-  },
+  template: `{{message}}`,
 });
 vm.mount("#app");
 // mounting seprately to look clean and easy to read //
+
+let vm2 = Vue.createApp({
+  data() {
+    return {
+      message: "Hello world!",
+    };
+  },
+  render() {
+    return Vue.h("h1", this.message);
+  },
+}).mount("#app2");
